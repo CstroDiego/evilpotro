bafc28f84fb155b8ac70ca95d346fbcf9600173c
eljuls22 <julio.blanco228442@potros.itson.edu.mx>
2022-12-01 09:07:42 -0700

    Creación de las clases

---
+++ src/mx/itson/evilpotro/entidades/Curso.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.evilpotro.entidades;
+
+/**
+ *
+ * @author julio
+ */
+public class Curso {
+ 
+    private int id;
+    private String titulo;
+    private int semestre;
+    private String bloque;
+    private String carrera;
+    
+}
---
+++ src/mx/itson/evilpotro/entidades/RegistroAcademico.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.evilpotro.entidades;
+
+/**
+ *
+ * @author julio
+ */
+public class RegistroAcademico {
+ 
+    private Alumno alumno;
+    private Curso curso;
+    private int calificacion;
+    private String cicloLectivo;
+    private int aprobado;
+    
+}
