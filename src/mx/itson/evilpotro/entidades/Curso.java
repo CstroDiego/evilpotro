7519b73f8d81d1c471818464653dba26cd2910e6
eljuls22 <julio.blanco228442@potros.itson.edu.mx>
2022-12-01 18:40:42 -0700

    Getters y setters de las entidades

--- src/mx/itson/evilpotro/entidades/Curso.java
+++ src/mx/itson/evilpotro/entidades/Curso.java
@@ -9,11 +9,81 @@ package mx.itson.evilpotro.entidades;
  * @author julio
  */
 public class Curso {
- 
+
     private int id;
     private String titulo;
     private int semestre;
     private String bloque;
     private String carrera;
-    
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the semestre
+     */
+    public int getSemestre() {
+        return semestre;
+    }
+
+    /**
+     * @param semestre the semestre to set
+     */
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    /**
+     * @return the bloque
+     */
+    public String getBloque() {
+        return bloque;
+    }
+
+    /**
+     * @param bloque the bloque to set
+     */
+    public void setBloque(String bloque) {
+        this.bloque = bloque;
+    }
+
+    /**
+     * @return the carrera
+     */
+    public String getCarrera() {
+        return carrera;
+    }
+
+    /**
+     * @param carrera the carrera to set
+     */
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
 }
--- src/mx/itson/evilpotro/entidades/RegistroAcademico.java
+++ src/mx/itson/evilpotro/entidades/RegistroAcademico.java
@@ -9,11 +9,81 @@ package mx.itson.evilpotro.entidades;
  * @author julio
  */
 public class RegistroAcademico {
- 
+
     private Alumno alumno;
     private Curso curso;
     private int calificacion;
     private String cicloLectivo;
     private int aprobado;
-    
+
+    /**
+     * @return the alumno
+     */
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    /**
+     * @param alumno the alumno to set
+     */
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    /**
+     * @return the curso
+     */
+    public Curso getCurso() {
+        return curso;
+    }
+
+    /**
+     * @param curso the curso to set
+     */
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    /**
+     * @return the cicloLectivo
+     */
+    public String getCicloLectivo() {
+        return cicloLectivo;
+    }
+
+    /**
+     * @param cicloLectivo the cicloLectivo to set
+     */
+    public void setCicloLectivo(String cicloLectivo) {
+        this.cicloLectivo = cicloLectivo;
+    }
+
+    /**
+     * @return the aprobado
+     */
+    public int getAprobado() {
+        return aprobado;
+    }
+
+    /**
+     * @param aprobado the aprobado to set
+     */
+    public void setAprobado(int aprobado) {
+        this.aprobado = aprobado;
+    }
+
 }
